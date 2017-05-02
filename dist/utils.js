66175089b111917e6090eec728b3238c849f587f
Nick Hoffman <njhoffman1982@gmail.com>
2017-05-02 18:54:30 -0500

    rebuilt dist files

--- dist/utils.js
+++ dist/utils.js
@@ -1,5 +1,10 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.isSerializable = exports.getMaxIndexLength = exports.indent = undefined;
+
 var _getOwnPropertyNames = require('babel-runtime/core-js/object/get-own-property-names');
 
 var _getOwnPropertyNames2 = _interopRequireDefault(_getOwnPropertyNames);
@@ -8,25 +13,23 @@ var _lodash = require('lodash');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.indent = function indent(numSpaces) {
+var indent = exports.indent = function indent(numSpaces) {
   return new Array(numSpaces + 1).join(' ');
 };
 
-exports.getMaxIndexLength = function (input) {
+var getMaxIndexLength = exports.getMaxIndexLength = function getMaxIndexLength(input) {
   var maxWidth = 0;
-
   (0, _getOwnPropertyNames2.default)(input).forEach(function (key) {
     if (input[key] === undefined) {
       return;
     }
-
     maxWidth = Math.max(maxWidth, key.length);
   });
   return maxWidth;
 };
 
 // detect if an object can be output on same line
-exports.isSerializable = function (input, inlineArrays) {
+var isSerializable = exports.isSerializable = function isSerializable(input, inlineArrays) {
   if ((0, _lodash.isBoolean)(input) || (0, _lodash.isNumber)(input) || (0, _lodash.isFunction)(input) || (0, _lodash.isUndefined)(input) || (0, _lodash.isNull)(input) || (0, _lodash.isString)(input) && input.indexOf('\n') === -1 || input instanceof Date) {
     return true;
   }
@@ -36,14 +39,15 @@ exports.isSerializable = function (input, inlineArrays) {
     return true;
   }
 
-  if (inlineArrays && Array.isArray(input) && exports._isSerializable(input[0], false)) {
+  if (inlineArrays && (0, _lodash.isArray)(input) && isSerializable(input[0], false)) {
     return true;
   }
   return false;
 };
 
-exports.stripAnsi = function (data) {
-  var ansiRE = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
-  return data.replace(ansiRE, '');
+module.exports = {
+  indent: indent,
+  getMaxIndexLength: getMaxIndexLength,
+  isSerializable: isSerializable
 };
 //# sourceMappingURL=utils.js.map
