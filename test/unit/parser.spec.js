b817d6aa82156a7f99e9bdd0a7d9ab5c93df0c87
Nick Hoffman <njhoffman1982@gmail.com>
2017-05-11 00:15:14 -0500

    added tests for parser

--- test/unit/parser.spec.js
+++ test/unit/parser.spec.js
@@ -1,19 +1,19 @@
 describe('Parser', () => {
 
-  let sandbox, parser, pColorStub, keyColorStub;
-  describe('customColor indexes', () => {
+  let sandbox, parser;
+  describe('CustomColor Indexes', () => {
     let options = { customColors : { testColor: { fg: [1,2,3] } } };
+    let pColorStub;
     beforeEach(() => {
       sandbox = sinon.sandbox.create();
-      pColorStub = sandbox.stub().returns('colorized string');
+      pColorStub = sandbox.stub().returns('special colorized string');
       parser = proxyquire('../lib/parser', {
         './settings': {
           getOptions: () => options,
           getPrintColor: () => ({
-            testColor: pColorStub,
-            keys: () => 'colorized keys'
+            testColor: pColorStub
           })
-        },
+        }
       });
     });
 
@@ -22,17 +22,17 @@ describe('Parser', () => {
     });
 
 
-    it('Should correctly identify object keys that match initialized customColrs', () => {
+    it('Should correctly identify object keys that match initialized customColors', () => {
       const input = { testColor: 'This should be colorized' };
       parser(input);
       expect(pColorStub).to.be.called.once;
       expect(pColorStub).to.be.calledWith('This should be colorized');
     });
-    //
+
     it('Should colorize the value of matched customColor keys of input object', () => {
       const input = { testColor: 'This should be colorized' };
       const output = parser(input);
-      expect(output).to.equal('colorized string');
+      expect(output).to.equal('special colorized string');
     });
 
 
@@ -42,8 +42,84 @@ describe('Parser', () => {
 
   // describe('object handling', () => {
   // });
-  // describe('serializable data handling', () => {
-  // });
+  describe('Serializable Data', () => {
+    let options = { inlineArrays: true };
+    let stringColorStub, falseColorStub, trueColorStub, numberColorStub, emptyColorStub, dateColorStub;
+
+    beforeEach(() => {
+      sandbox = sinon.sandbox.create();
+      stringColorStub = sandbox.stub().returns('string color');
+      falseColorStub = sandbox.stub().returns('false color');
+      trueColorStub = sandbox.stub().returns('true color');
+      numberColorStub = sandbox.stub().returns('number color');
+      emptyColorStub = sandbox.stub().returns('empty color');
+      dateColorStub = sandbox.stub().returns('date color');
+      parser = proxyquire('../lib/parser', {
+        './settings': {
+          getOptions: () => options,
+          getPrintColor: () => ({
+            string: stringColorStub,
+            boolTrue: trueColorStub,
+            boolFalse: falseColorStub,
+            number: numberColorStub,
+            empty: emptyColorStub,
+            date: dateColorStub
+          })
+        }
+      });
+    })
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it('Should format string correctly', () => {
+      const output = parser('testing string');
+      expect(output).to.equal('string color');
+      expect(stringColorStub).to.be.called.once;
+    });
+
+    it('Should format number correctly', () => {
+      const output = parser(5);
+      expect(output).to.equal('number color');
+      expect(numberColorStub).to.be.called.once;
+    });
+
+    it('Should format true correctly', () => {
+      const output = parser(true);
+      expect(output).to.equal('true color');
+      expect(trueColorStub).to.be.called.once;
+    });
+
+    it('Should format false correctly', () => {
+      const output = parser(false);
+      expect(output).to.equal('false color');
+      expect(falseColorStub).to.be.called.once;
+    });
+
+    it('Should format null correctly', () => {
+      const output = parser(null);
+      expect(output).to.equal('empty color');
+      expect(emptyColorStub).to.be.called.once;
+    });
+
+    it('Should format a date correctly', () => {
+      const output = parser(new Date());
+      expect(output).to.equal('date color');
+      expect(dateColorStub).to.be.called.once;
+    });
+
+    it('Should format an inline array correctly', () => {
+      const output = parser(['arr1', 'arr2', 'arr3']);
+      expect(output).to.equal('arr1, arr2, arr3');
+    });
+
+    it('Should format other unknown types correctly', () => {
+      const output = parser(undefined);
+      expect(output).to.equal('undefined');
+    });
+
+  });
   // describe('multiline strings', () => {
   // });
 });
