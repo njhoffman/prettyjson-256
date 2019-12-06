cb697f2bef70d66e5e1e0f75df339ac0ffe52c44
Nicholas Hoffman <njhoffman1982@gmail.com>
2019-12-06 13:28:53 -0600

    Change lodash usage and fix a few more lint errors

--- test/unit/other.js
+++ test/unit/other.js
@@ -1,1 +1,27 @@
-require('../integration/integration');
+// require('../integration/integration');
+const pjson = require('../../lib/prettyjson');
+
+const a = {
+  name: {
+    age: {
+      name: 'name',
+      three: 'three',
+      next: {
+        another: {
+          hey: 'hey',
+          bye: 'bye'
+        }
+      }
+    }
+  }
+};
+
+pjson.init({
+  depth: 3
+});
+
+console.log(pjson.render(a));
+console.log('=====================================');
+console.log(pjson.render(a, 2, { depth: 2 }));
+console.log('=====================================');
+console.log(pjson.render(a));
